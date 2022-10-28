75b8292fc1a40b1d3472d70b7fadb98a7a307871
Andres felipe estrada cortes <afecestra@hotmail.com>
2022-10-28 07:43:52 -0500

    metodo actualizar, eliminar

--- server/controllers/producto.controller.js
+++ server/controllers/producto.controller.js
@@ -20,4 +20,17 @@ module.exports.create_product = (req, res) => {
         .then(producto => res.json(producto))
         .catch(err => res.json({message: "Hubo un error "+err}));
 }
+//el new true regresa el documento que se esta modificando
+// runValidators: true nos revisa las vallidaciones que vamos a tener 
+module.exports.update_producto = (req, res) =>{
+    Producto.findOneAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators:true})
+        .then(producto => res.json(producto))
+        .catch(err => res.json({message: "Hubo un error "+err}));
+}
+
+module.exports.delete_producto = (req, res) =>{
+    Producto.deleteOne({_id: req.params.id})
+    .then(result => res.json({result}))
+    .catch(err => res.json({message: "Hubo un error "+err}));
+}
 
